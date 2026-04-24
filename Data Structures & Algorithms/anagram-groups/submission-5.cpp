09fb23d0514cb8df4891f3efec07728719eb616b
Xiang <150136430+yuniopq@users.noreply.github.com>
2026-04-24 14:17:15 +0200

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mapa;
+        
+        for (const auto& s:strs){
+            int contador[26]={0};
+            for (const auto& c:s){
+                contador[c-'a']++;
+            }
+            string str_contador;
+            for (int i = 0; i< 26; i++){
+                str_contador+=to_string(contador[i]) + ",";
+            }
+            mapa[str_contador].push_back(s);
+        }
+        vector<vector<string>> res;
+        for(auto& pair : mapa)
+        {
+            res.push_back(pair.second);
+        }
+        return res;
+    }
+};
